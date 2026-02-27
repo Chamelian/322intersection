6cfb8c502ea985833ceaff863180da1984c4169b
John Maynard <jmaynard2@mtech.edu>
2026-02-27 12:13:25 -0700

    Format files correctly

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+John Maynard
+CSCI 332 Spring 2025
+Programming Assignment #class17
+I acknowledge that I have worked on this assignment independently, except where explicitly noted and referenced. Any collaboration or use of external resources has been properly cited. I am fully aware of the consequences of academic dishonesty and agree to abide by the university's academic integrity policy. I understand the importance the consequences of plagiarism.
+"""
+
 def add_positive_integers(a: int, b: int) -> int:
     if not isinstance(a, int) or not isinstance(b, int):
         raise ValueError("Inputs must be integers")
--- tests.py
+++ tests.py
@@ -1,3 +1,10 @@
+"""
+John Maynard
+CSCI 332 Spring 2025
+Programming Assignment #class17
+I acknowledge that I have worked on this assignment independently, except where explicitly noted and referenced. Any collaboration or use of external resources has been properly cited. I am fully aware of the consequences of academic dishonesty and agree to abide by the university's academic integrity policy. I understand the importance the consequences of plagiarism.
+"""
+
 import unittest
 from main import add_positive_integers
 
@@ -11,7 +18,7 @@ class TestMathFunctions(unittest.TestCase):
 
     def test_wrong_type(self):
         with self.assertRaises(ValueError):
-            add_positive_integers("5", 5)
+            add_positive_integers("5", 5) # type: ignore
 
 if __name__ == "__main__":
     unittest.main()
